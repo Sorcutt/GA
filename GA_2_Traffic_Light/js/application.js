10991f572d8ff9c900b11b9fe04ce66e87a876f1
ShannonOrcutt <shannon.orcutt88@gmail.com>
2014-09-18 21:33:44 -0400

    slight changes to html and css, javascript started

---
+++ GA_2_Traffic_Light/js/application.js
@@ -0,0 +1,7 @@
+function buttonClick(chosenLight){
+	var redLight = document.getElementById('red-light');
+	var yellowLight = document.getElementById('yellow-light');
+	var greenLight = document.getElementById('green-light');
+
+	
+}
