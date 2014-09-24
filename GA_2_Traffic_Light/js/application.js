8a7879d86d4e537558118582413dc934c4dc4f79
ShannonOrcutt <shannon.orcutt88@gmail.com>
2014-09-23 21:37:15 -0400

    traffic light finalized and functional

--- GA_2_Traffic_Light/js/application.js
+++ GA_2_Traffic_Light/js/application.js
@@ -3,5 +3,18 @@ function buttonClick(chosenLight){
 	var yellowLight = document.getElementById('yellow-light');
 	var greenLight = document.getElementById('green-light');
 
-	
+	yellowLight.style.backgroundColor = "#252525";
+	redLight.style.backgroundColor = "#252525";
+	greenLight.style.backgroundColor = "#252525";
+
+	var whichLight = document.getElementById(chosenLight);
+	if (chosenLight == 'red-light') {
+		var color = '#ff0000';
+	} else if (chosenLight == 'yellow-light') {
+		var color = '#fffc00';
+	} else {
+		var color = '#007d18';
+	}
+
+	whichLight.style.backgroundColor = color;
 }
