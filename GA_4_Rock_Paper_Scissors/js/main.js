8944fd62123d4c605c7fde216ac14516a35debbf
ShannonOrcutt <shannon.orcutt88@gmail.com>
2014-10-10 18:47:42 -0400

    rock paper sscissors page styled

---
+++ GA_4_Rock_Paper_Scissors/js/main.js
@@ -0,0 +1,32 @@
+
+
+var userChoice = "paper";
+var computerChoice = "rock";
+
+//combined string outcomes:
+var userWins = ["rockscissors", "paperrock", "scissorspaper"];
+
+//generate computer choice:
+var choices = ["rock", "paper", "scissors"];
+
+computerChoice = choices[Math.random()*choices.length];
+
+function evaluate() {
+	var msg = "Computer Wins!";
+
+	if(userChoice==computerChoice) {
+		msg = "Tie!";
+	} else {
+
+		var outCome = userChoice + computerChoice;
+		$.each(userWins, function(index, value) {
+			if(outCome==value){
+				msg = "You Win!"
+			} 
+		});
+		return msg;
+	}
+}
+
+var outComeMsg = evaluate();
+alert(outComeMsg);
